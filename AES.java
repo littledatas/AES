90cbc63f2b349730037d3e0cf0eb63ecdb830976
Nishil <nishil94@gmail.com>
2014-11-02 15:28:02 -0600

    Added subBytes, shiftRows, mixColumns, and addRoundKey methods

--- AES.java
+++ AES.java
@@ -47,6 +47,74 @@ public class AES {
         }
 
     }
+    /*For each byte in the array, use its value as an index into a fixed 256-element lookup table, 
+      and replace its value in the state by the byte value stored at that location in the table.*/  
+    private int[][] subBytes(int[][] input)
+    {
+        for(int row = 0; row < input.length; row++)
+        {
+            for(int col = 0; row < input[0].length; col++)
+            {
+                int leftdigit = input[row][col]>>>4;
+                int rightdigit = input[row][col] & 0x000F; 
+                input[row][col] = sbox[leftdigit*16+rightdigit];
+            }
+        }
+        return input;
+    } 
+
+    /* Let Ri denote the ith row in state. Shift R0 in the state left 0 bytes (i.e., no change); 
+       shift R1 left 1 byte; shift R2 left 2 bytes; shift R3 left 3 bytes. These are circular shifts. 
+       They do not affect the individual byte values themselves.*/
+    private int[][] shiftRows(int[][] input)
+    {
+        //Create a new 4x4 array with the same size as the input array
+        int[][] result = new int[input.length][input[0].length];
+
+        for(int row = 0; row < input.length; row++)
+        {
+            for(int col = 0; row < input[0].length; col++)
+            {
+                /*We shift all values in a row n times where n is the row number.*/
+                    int newcol = col - row;
+                    if(newcol<0)
+                        newcol += input.length;
+                    result[row][newcol] = input[row][col];
+            }
+        }
+
+        return result;
+    } 
+    
+    /*For each column of the state, replace the column by its value multiplied 
+      by a fixed 4 x 4 matrix of integers (in a particular Galois Field). This is 
+      the most complex step. Find details at any of several websites. Note that 
+      the inverse operation multiplies by a different matrix.*/
+    private int[][] mixColumns(int[][] input)
+    {
+        //input should be a 4x1 matrix
+        int[][] result = new int[input.length][input[0].length];
+        result[0][0] = 2*input[0][0] + 3*input[1][0] + input[2][0] + input[3][0];
+        result[1][0] = input[0][0] + 2*input[1][0] + 3*input[2][0] + input[3][0];
+        result[2][0] = input[0][0] + input[1][0] + 2*input[2][0] + 3*input[3][0];
+        result[3][0] = 3*input[0][0] + input[1][0] + input[2][0] + 2*input[3][0];
+        return result; 
+    }
+    /*XOR the state with a 128-bit round key derived from the original key K by
+      a recursive process.*/
+    private int[][] addRoundKey(int[][] input, int[][] roundkey)
+    {
+        int[][] result = new int[input.length][input[0].length];
+        for(int row = 0; row < input.length; row++)
+        {
+            for(int col = 0; col < input[0].length; col++)
+            {
+                result[row][col] = input[row][col] ^ roundkey[row][col];
+            }
+        }
+        return result;
+    }
+
 
     private boolean cmdLine_valid(String[] args) {
         String process = args[0].toLowerCase();
